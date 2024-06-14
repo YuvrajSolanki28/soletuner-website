b4f4c9276d22014915824af470cc21fc2f68cc7d
yuvrajsolanki <yuvrajsolanki2809@gmail.com>
2024-06-14 19:58:23 +0530

    R

--- src/helpers/DataFromToken.js
+++ src/helpers/DataFromToken.js
@@ -2,7 +2,7 @@ import { NextRequest } from "next/server";
 import jwt from 'jsonwebtoken'
 
 
-export const getDataFromToken = (request: NextRequest) => {
+export const getDataFromToken = (request) => {
     try {
         const token = request.cookies.get("token")?.value || "";
         jwt.verify
